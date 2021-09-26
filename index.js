0403ccd9cbbea44258856b1a4ba6b587a04cc294
Sahiba Kumari <sahibakumari0915@outlook.com>
2021-09-26 14:26:06 +0530

    feat: changed the html file to js file

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Do-You-Know-Sahiba</title>
-</head>
-<body>
-  <script>
-  var readlineSync = require('readline-sync');
+var readlineSync = require('readline-sync');
 
 
 console.log("Welcome to the game, Do you know Sahiba? \n")
@@ -107,10 +97,6 @@ console.log("3. black ");
 console.log("4. butter scotch ");
 console.log("5. 0 ");
 }
-</script>
-  
-</body>
-</html>
 
 
 
